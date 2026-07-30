7145f3e35328ade7b8354bed4740738d02beb443
Vikash kushwaha <152359232+vikash0064@users.noreply.github.com>
2026-07-30 15:58:28 +0530

    Added Longest Substring Without Repeating Characters [LeetCode] (Medium)

---
+++ LeetCode/hash-table/longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        unordered_map<char, int> mp;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
