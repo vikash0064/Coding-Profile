10ab4c47deceb971738b6741b4daf9de5d88affb
Vikash kushwaha <152359232+vikash0064@users.noreply.github.com>
2026-07-31 07:47:35 +0530

    Added Zigzag Conversion [LeetCode] (Medium)

--- LeetCode/string/zigzag-conversion/solution.cpp
+++ LeetCode/string/zigzag-conversion/solution.cpp
@@ -1,1 +1,22 @@
-c
+        for (char ch : s) {
+            rows[currentRow] += ch;
+
+            if (currentRow == 0)
+                goingDown = true;
+            else if (currentRow == numRows - 1)
+                goingDown = false;
+
+            if (goingDown)
+                currentRow++;
+            else
+                currentRow--;
+        }
+
+        string ans = "";
+
+        for (string row : rows)
+            ans += row;
+
+        return ans;
+    }
+};
